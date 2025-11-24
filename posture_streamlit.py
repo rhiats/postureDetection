25ec24e88d980b2f73119881a1fad05d797ecf89
Rhia Singh <rhia.singh22@gmail.com>
2025-11-24 13:14:57 -0500

    Requirement file

--- posture_streamlit.py
+++ posture_streamlit.py
@@ -58,3 +58,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
         # Display frame in Streamlit
         frame_placeholder.image(frame, channels="BGR")
+
+stop = st.button("Stop Camera")
+if stop:
+    break
